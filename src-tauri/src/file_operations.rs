8283e3f45f0cff1e3b03557b3b18edf532fd42b3
Michael Mathews <mmmathe4@asu.edu>
2024-11-15 13:33:41 -0700

    Put auxilary function separate.

--- src-tauri/src/file_operations.rs
+++ src-tauri/src/file_operations.rs
@@ -1,5 +1,6 @@
 use std::fs::{File, create_dir_all, metadata};
 use std::io::Write;
+use std::path::PathBuf;
 use dirs;
 
 pub fn create_story_files() -> Result<(), String> {
@@ -9,43 +10,38 @@ pub fn create_story_files() -> Result<(), String> {
     // Define the folder and file path
     let effort_logger_dir = home_dir.join("effort-logger");
     let story_file_path = effort_logger_dir.join("story-logs.txt");
+    let story_list_path = effort_logger_dir.join("story-list.txt");
 
     // Create the "effort-logger" folder if it doesn't exist
     if !effort_logger_dir.exists() {
         create_dir_all(&effort_logger_dir).map_err(|e| format!("Failed to create directory: {}", e))?;
     }
 
+    let _ = create_file(&story_file_path, b"Story ID, Start DateTime, End DateTime, Duration\n");
+    let _ = create_file(&story_list_path, b"Story ID\n");
+
+    Ok(())
+}
+
+fn create_file(file_path: &PathBuf, initial_data:&[u8]) -> Result<(), String> {
+    /* Create a file
+     * 
+     * file_path: The path of the file to be created
+     * initial_data: The initial data to be written to the file in binary format.
+     */
     // Check if the story-list.txt file already exists
-    if metadata(&story_file_path).is_ok() {
+    if metadata(&file_path).is_ok() {
         // If the file exists, do nothing and return
         return Ok(());
     }
-    // If the file doesn't exist, create it. 
     else {
+        // If the file doesn't exist, create it. 
         // Create the story-list.txt file
-        let mut file = File::create(story_file_path).map_err(|e| format!("Failed to create file: {}", e))?;
+        let mut file = File::create(file_path).map_err(|e| format!("Failed to create file: {}", e))?;
     
         // Optionally, write initial data to the file
-        file.write_all(b"Story ID, Start DateTime, End DateTime, Duration\n")
+        file.write_all(initial_data)
             .map_err(|e| format!("Failed to write to file: {}", e))?;
     }
-
-    let story_list_path = effort_logger_dir.join("story-list.txt");
-
-    // Check if the story-list.txt file already exists
-    if metadata(&story_list_path).is_ok() {
-        // If the file exists, do nothing and return
-        return Ok(());
-    }
-    // If the file doesn't exist, create it. 
-    else {
-        // Create the story-list.txt file
-        let mut file = File::create(story_list_path).map_err(|e| format!("Failed to create file: {}", e))?;
-    
-        // Optionally, write initial data to the file
-        file.write_all(b"Story ID\n")
-            .map_err(|e| format!("Failed to write to file: {}", e))?;
-    }
-
     Ok(())
 }
